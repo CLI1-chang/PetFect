c1e779ae300ae8438712ea8d542738cc2146945f
zengxi <zengxi@zengxis-MacBook-Pro.local>
2021-10-27 12:23:33 -0700

    check in front end page design for home, about etc

--- app/main/views.py
+++ app/main/views.py
@@ -28,7 +28,10 @@ def index():
 def about():
     return render_template("about.html")
 
-
 @main.route('/animal')
 def animal():
     return render_template("animal.html")
+
+@main.route('/contact')
+def contact():
+    return render_template("contact.html")
